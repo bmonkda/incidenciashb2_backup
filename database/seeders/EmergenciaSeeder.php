77ce6b881b864ffc60dfe353819ae8caf294fd2c
erwilgomez14 <erwildiaz@gmail.com>
2023-05-15 08:21:19 -0400

    Agregando seeder de emergencia

--- database/seeders/EmergenciaSeeder.php
+++ database/seeders/EmergenciaSeeder.php
@@ -14,6 +14,22 @@ class EmergenciaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $emergency = Emergencia::create([
+            'nombre' => 'BAJA',
+            'color' => 'warning',
+            'color2' => '#e2a03f',
+        ]);
+
+        $emergency = Emergencia::create([
+            'nombre' => 'MEDIA',
+            'color' => 'secondary',
+            'color2' => '#d1d1d1',
+        ]);
+
+        $emergency = Emergencia::create([
+            'nombre' => 'ALTA',
+            'color' => 'danger',
+            'color2' => '#e7515a',
+        ]);
     }
 }
