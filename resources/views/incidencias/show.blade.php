a4f9b3a73623489cdc8b1674957999a575404b5f
JOSE <bmonkdareg@gmail.com>
2023-06-06 23:17:35 -0400

    Creando la vista incidencias.show con la plantilla (solo recibe los datos)

---
+++ resources/views/incidencias/show.blade.php
@@ -0,0 +1,52 @@
+@extends('template.layouts.page')
+
+{{-- @section('styles')
+    <link href="{{asset('template/assets/css/elements/infobox.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('template/assets/css/components/custom-counter.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('template/assets/css/scrollspyNav.css')}}" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="{{asset('template/assets/css/forms/theme-checkbox-radio.css')}}">
+    <link href="{{asset('template/assets/css/tables/table-basic.css')}}" rel="stylesheet" type="text/css" />
+@endsection --}}
+
+
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{asset('template/plugins/table/datatable/datatables-dark.css')}}">
+    {{-- <link rel="stylesheet" type="text/css" href="{{asset('template/plugins/table/datatable/dt-global_style-dark.css')}}"> --}}
+    <link href="{{asset('template/assets/css/elements/tooltip.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('template/plugins/tagInput/tags-input.css')}}" rel="stylesheet" type="text/css" />
+@endsection
+
+
+@section('content')
+
+<div class="row mt-5">
+    <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+        <h4>Detalles de la incidencia</h4> {{ $incidencia }}
+    </div>
+</div>
+
+
+@endsection
+
+@section('scripts')
+    <script src="{{asset('template/plugins/counter/jquery.countTo.js')}}"></script>
+    <script src="{{asset('template/assets/js/components/custom-counter.js')}}"></script>
+
+    <script src="{{asset('template/assets/js/elements/tooltip.js')}}"></script>
+    <script src="{{asset('template/plugins/table/datatable/datatables.js')}}"></script>
+    <script>
+        $('#zero-config').DataTable({
+            "oLanguage": {
+                "oPaginate": { "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>', "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>' },
+                "sInfo": "Mostrando pagina _PAGE_ of _PAGES_",
+                "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
+                "sSearchPlaceholder": "Buscar...",
+                "sLengthMenu": "Cantidad de resgistros :  _MENU_",
+            },
+            "stripeClasses": [],
+            "lengthMenu": [7, 10, 20, 50],
+            "pageLength": 7
+        });
+    </script>
+
+@endsection
