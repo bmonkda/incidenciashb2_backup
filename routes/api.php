091fd8522c8b979e7c5308b8c421541d74987694
JOSE <bmonkdareg@gmail.com>
2023-06-08 23:09:25 -0400

    Manejando ruta API para obtener las subcategorias de una categoria

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SubcategoriaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/subcategorias/{categoria}', [SubcategoriaController::class, 'getByCategoria']);
