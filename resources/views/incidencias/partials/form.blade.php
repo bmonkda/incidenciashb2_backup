f4ee3795252387b9c46cc6a1c0d0b4413c12402c
JOSE <bmonkdareg@gmail.com>
2023-06-08 23:04:40 -0400

    Formulario de incidencia

--- resources/views/incidencias/partials/form.blade.php
+++ resources/views/incidencias/partials/form.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <label for="titulo">Título:</label>
+    <label for="titulo">Título*:</label>
     <input type="text" name="titulo" id="titulo" class="form-control" placeholder="Ingrese el título de la incidencia">
 
     @error('titulo')
@@ -25,3 +25,43 @@
     @enderror
 </div>
 
+<div class="form-group">
+    <label>Categoría*</label>
+    <select class="form-control" name="categoria_id" id="categoriaSelect">
+        {{-- <option value="">Seleccione una categoría</option> --}}
+        <option selected disabled>Selecionar Categoria</option>
+        @foreach ($subcategorias->unique('categoria_id') as $subcategoria)
+            <option value="{{ $subcategoria->categoria->id }}">{{ $subcategoria->categoria->id }} - {{ $subcategoria->categoria->nombre }}</option>
+        @endforeach
+    </select>
+
+    @error('categoria_id')
+        <small class="text-danger">{{ $message }}</small>
+    @enderror
+</div>
+
+
+<div class="form-group">
+    <label>Subcategoría*</label>
+    <select class="form-control" name="subcategoria_id" id="subcategoriaSelect">
+        <option value="">Debe seleccionar una Categoría</option>
+    </select>
+
+    @error('subcategoria_id')
+        <small class="text-danger">{{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label>Urgencia*</label>
+    <select class="form-control" name="emergency_id" id="emrgencia">
+        <option selected disabled>Selecionar Urgencia</option>
+        @foreach ($emergencias as $emergencia)
+            <option value="{{ $emergencia->id }}">{{ $emergencia->id }} - {{ $emergencia->nombre }}</option>
+        @endforeach
+    </select>
+
+    @error('categoria_id')
+        <small class="text-danger">{{ $message }}</small>
+    @enderror
+</div>
